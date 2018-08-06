984dee46009cd5a44f526cf8ced460ab2ddb92ac
Madeline Celis <madelinebcelis@gmail.com>
2018-08-06 12:29:02 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -55,7 +55,7 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  cart = consolidate_cart(cart:[])
+  cart = consolidate_cart(cart)
   cart = apply_coupons(cart:[], coupons:[])
   cart = apply_clearance(cart:[])
   binding.pry
